c34940e337f4268ddd08cdd4c39cfdcb7358deb4
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2021-12-02 15:11:31 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ const express = require("express");
 const fs = require("fs");
 
 const client = new discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES" ]});
-client.on("ready", () => console.log("Logged in!"));
+
+client.on("ready", () => {
+  const { REST } = require("@discordjs/rest");
+  const { Routes } = require("discord-api-types/v9");
+  
+  const commands = [];
+  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) commands.push(require(`./commands/${file}`).data.toJSON());
+  
+  const rest = new REST({ version: "9" }).setToken(process.env.token)
+  client.guilds.cache.forEach(guild => rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guild.id), { body: commands }));
+});
+
 client.login(process.env.TOKEN);
 
 const app = express();
