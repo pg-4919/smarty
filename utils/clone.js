01c91cf87d6d4855cb0ae60f0aaa6a16f2cb39e1
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-04-22 22:49:08 -0400

    homosexual

--- utils/clone.js
+++ utils/clone.js
@@ -1,3 +1,5 @@
+const discord = require("discord.js");
+
 module.exports = {
     async clone(member, channel) {
         const webhook = await channel.createWebhook(member.nickname || member.user?.username || "Trump", {
