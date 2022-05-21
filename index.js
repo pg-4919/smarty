0939f26eb3778e2671de0b886af772d9e220712b
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-05-21 17:11:27 -0400

    gay

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function updateCommands() {
 }
 
 function sleep(ms) {
-    return new Promise((resolve, reject) => setTimeout(ms, resolve));
+    return new Promise((resolve, reject) => setTimeout(resolve, ms));
 }
 
 async function rainbow(role) {
