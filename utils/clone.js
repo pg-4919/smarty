02767382b1a53fe802a3bd19c8c339179eff7908
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-06-09 10:36:31 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -4,7 +4,10 @@ module.exports = {
     async clone(member, channel, message) {
         const webhooks = await channel.fetchWebhooks();
         const webhook = webhooks.first() || await channel.createWebhook("Smarty Internals");
+        const attachments = message.attachments.toJSON();
+        
         await webhook.send({
+            attachments: attachments,
             content: message.content,
             username: member.displayName,
             avatarURL: member.displayAvatarURL(),
