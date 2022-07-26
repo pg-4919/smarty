b288b50eba1f873d7886bc5a91c18914a836dbe0
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-07-26 08:22:50 -0400

    gay

--- index.js
+++ index.js
@@ -44,9 +44,10 @@ client.on("messageCreate", async message => {
 
     const currentStats = JSON.parse(fs.readFileSync("./data/stats.json"));
     currentStats[author.id] = currentStats[author.id] || {};
-    currentStats[author.id].sent = currentStats[author.id].sent || {};
+    currentStats[author.id].sent = currentStats[author.id].sent || 0;
+    currentStats[author.id].sent += 1;
     console.log(currentStats);
-    //fs.writeFileSync("./data/stats.json", JSON.stringify(currentStats));
+    fs.writeFileSync("./data/stats.json", JSON.stringify(currentStats));
 });
 
 client.on("channelPinsUpdate", async (channel, time) => {
