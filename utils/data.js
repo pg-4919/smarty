1a051f60a1d982c8c94fc34f88f62fd534e8a5a1
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-07-26 07:42:36 -0400

    gay

--- utils/data.js
+++ utils/data.js
@@ -8,7 +8,7 @@ module.exports = {
         const exec = require("child_process").exec;
 
         return new Promise((resolve, reject) => {
-            exec(`cd ${root} && cd ../ && ./start`, (err, out) => {
+            exec(`cd ${root} && cd ../ && ./update-repo`, (err, out) => {
                 if (error) return reject(error);
                 resolve(stdout);
             });
