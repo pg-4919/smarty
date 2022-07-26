a22a65c597be1ce85a8ac3f513d15020b9ea2888
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-07-26 07:28:54 -0400

    gay

--- utils/data.js
+++ utils/data.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
 const path = require("path");
-/*
+const root = require("./root.js");
+
+function updateBotData() {
+    const { exec } = require("child_process");
+
+    exec("")
+}
+
 module.exports = {
+    read(datafile) {
+        return fs.readFileSync(`${root}/data/${datafile}`);
+    },
     write(datafile, contents) {
-        fs.writeFileSync(require.main.filename);
+        return fs.writeFileSync(`${root}/data/${datafile}`, contents);
     }
 }
-*/
