96d9573387673db305a9ec0a92224335f7cebba1
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-07-26 07:53:30 -0400

    gay

--- commands/save.js
+++ commands/save.js
@@ -10,7 +10,7 @@ module.exports = {
         .toJSON(),
     async execute(interaction) {
         await interaction.deferReply();
-        const embed = utils.embed.default(
+        const embed = await utils.embed.default(
             `${await utils.data.updateRepo()}`,
             interaction.member,
             "saved the bot"
