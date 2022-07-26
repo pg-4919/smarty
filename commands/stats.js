c2040c704f0c6d4392108c317fb66607466b7392
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-07-26 16:58:57 -0400

    gay

--- commands/stats.js
+++ commands/stats.js
@@ -4,30 +4,34 @@ const utils = require("../utils/utils.js");
 
 module.exports = {
     name: "stats",
+
     data: new builders.SlashCommandBuilder()
         .setName("stats")
-        .setDescription("View your statistics in this server")
+        .setDescription("View someone's statistics")
         .addStringOption(option => 
             option.setName("statistic")
-                .setDescription("Which stat to view")
+                .setDescription("Which statistic to view")
                 .setRequired(true)
                 .addChoices(
                     { name: "Messages sent", value: "stats_msgs_sent" },
                     { name: "Commands executed", value: "stats_cmds_sent" }
                 )
         )
-        .addUserOption(option => option.setName("person").setDescription("Whom to check the stats of (defaults to you)").setRequired(false))
+        .addUserOption(option => option.setName("person")
+            .setDescription("Whom to check the stats of (defaults to you)")
+            .setRequired(false))
         .toJSON(),
+
     async execute(interaction) {
-        const statMessages = require(`${utils.path.assets}/statmsgs.json`);
-        const statId = interaction.options.getString("statistic").replace("stats_", "");
-        const target = interaction.options.getMember("person") || interaction.member;
-        const statValue = utils.stats.read(interaction.guild, target.user, statId);
+        const statistic = interaction.options.getString("statistic").replace("stats_", "");
+        const person = interaction.options.getMember("person") || interaction.member;
+        const value = utils.stats.read(interaction.guild, target.user, statId);
+        const template = require(`${utils.path.assets}/statmsgs.json`)[statistic];
 
         const embed = new discord.MessageEmbed()
             .setColor("#636363")
             .setTimestamp()
-            .setDescription(`<@${target.id}> ${statMessages[statId][0]} \`${statValue}\` ${statMessages[statId][1]}`)
+            .setDescription(`<@${target.id}> ${template[0]} \`${statValue}\` ${template[1]}`)
             .setFooter({ text: "checked some stats", iconURL: interaction.member.user.avatarURL() });
 
         await interaction.reply({ embeds: [embed] });
