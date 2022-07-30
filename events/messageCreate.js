143158e1f084ca760fa7cd03dadee52727969e6e
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-07-29 20:24:23 -0400

    gay

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,7 +1,7 @@
 const utils = require("../utils/utils.js");
 
 module.exports = async (message) => {
-    if (message.author.id === "614955640120410168") message.react("1001983320000839750").catch(() => {/* */})
+    //if (message.author.id === "614955640120410168") message.react("1001983320000839750").catch(() => {/* */})
     const author = message.author;
 
     if (author.id === message.client.user.id) return;
