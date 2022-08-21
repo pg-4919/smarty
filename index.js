61246f34b018d9d18d4e93b3af4eed41ddb930f1
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-08-21 11:33:43 -0400

    gay

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const utils = require("./utils/utils.js");
 const events = require("./events/events.js")
 const fs = require("fs");
 
-const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds] });
+const client = new discord.Client({ intents: new discord.IntentsBitField(32767) });
 
 client.commands = new discord.Collection(); //command files
 
