b6ab767e79311c48b2345c734b37c94ba7a8aa0f
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-04 16:45:30 -0400

    gay

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,10 +1,12 @@
 const utils = require("../utils/utils.js");
 
 module.exports = async (reaction, user) => {
-    await reaction.fetch();
-    console.log(reaction);
-    if (reaction.emoji.name !== "📌") return;
-    const starred = reaction.message.guild.channels.cache.find(channel => channel.name === "starred");
-    if (typeof starred === undefined) return;
-    return utils.clone(reaction.message.guild.members.cache.get(user.id), starred, reaction.message);
+    const message = reaction.message;
+    const guild = message.guild;
+    const starred = guild.channels.cache.find(channel => channel.name === "starred");
+
+    try { await reaction.fetch() } catch (err) { return err };
+    if (reaction.emoji.name !== "📌" || reaction.me || typeof starred === undefined) return;
+    await utils.clone(guild.members.cache.get(user.id), starred, message);
+    return message.react("✅");
 }
