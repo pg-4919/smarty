7b40e1f1775b658ba904edbe5a79eba417c56c68
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-04 16:34:55 -0400

    guild

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,7 +3,7 @@ const utils = require("../utils/utils.js");
 module.exports = async (reaction, user) => {
     console.log(reaction);
     if (reaction.emoji.name !== "pushpin") return;
-    const starred = guild.channels.cache.find(channel => channel.name === "starred");
+    const starred = reaction.message.guild.channels.cache.find(channel => channel.name === "starred");
     if (typeof starred === undefined) return;
     return utils.clone(reaction.message.guild.members.cache.get(user.id), starred, reaction.message);
 }
