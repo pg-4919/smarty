ee7178bfad810809bd775cec7488af3736340263
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-06 16:21:32 -0400

    gay

--- commands/color.js
+++ commands/color.js
@@ -6,35 +6,45 @@ module.exports = {
     data: new discord.SlashCommandBuilder()
         .setName("color")
         .setDescription("Change the color of your name")
-        .addStringOption(option => option.setName("hex").setDescription("The hex code of the color").setRequired(true))
+        .addStringOption(option => option
+            .setName("hex")
+            .setDescription("The hex code of the color")
+            .setRequired(true)
+        )
         .toJSON(),
     async respond(interaction) {
-        const hex = (interaction.options.getString("hex").replace("#", "") === "000000") ? "000001" : interaction.options.getString("hex").replace("#", "");
+        const hex = (interaction.options.getString("hex").replace("#", "") === "000000") ?
+            "000001" : interaction.options.getString("hex").replace("#", ""); //black hex replacement
         const embed = new discord.EmbedBuilder();
         const member = interaction.member;
         const guild = interaction.guild;
 
-        if (!/^[0-9A-F]{6}$/i.test(hex)) {
-            embed.setColor("#FF0000")
+        if (!/^[0-9A-F]{6}$/i.test(hex)) { //check if hex code is valid
+            embed
+                .setColor("#FF0000")
                 .setTimestamp()
                 .setDescription(`Not a valid hex code.`)
                 .setFooter({ text: "did a whoopsie", iconURL: member.user.avatarURL() });
         } else {
-
+            //find custom role & create role if none
             const customRole = member.roles.cache.filter(role => role.color !== 0).first() ||
                 await guild.roles.create({
                     name: member.displayName,
-                    position: guild.roles.cache.find(role => role.name === "Bots" && role.color === 0).position + 1
+                    position: guild.roles.cache.find(
+                        role => role.name === "Bots" && role.color === 0
+                    ).position + 1
                 });
-
+            
             member.roles.add(customRole);
             customRole.setColor(hex);
 
-            embed.setColor("#636363")
+            embed
+                .setColor("#636363")
                 .setTimestamp()
                 .setDescription(`You changed <@&${customRole.id}>'s color to #${hex}.`)
-                .setFooter({ text: "changed their color", iconURL: interaction.member.user.avatarURL() });
+                .setFooter({ text: "changed their color", iconURL: member.user.avatarURL() });
         }
+
         return interaction.reply({ embeds: [embed], ephemeral: true });
     }
 }
