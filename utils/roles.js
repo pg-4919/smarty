70bfd781b53adb81f321b2ff571b3b7031cdb8e9
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-06 17:04:32 -0400

    gay

--- utils/roles.js
+++ utils/roles.js
@@ -3,7 +3,7 @@ const root = require("./root.js")
 module.exports = {
     async purge(guild) {
         const roles = guild.roles.cache;
-        guild.members.fetch();
+        await guild.members.fetch();
         roles.each(async role => { 
             console.log(role.members.size)
         });
