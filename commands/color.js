ee7a189b038c0e9aab20d1a627e8e8d7770639d3
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-06 16:06:52 -0400

    gay

--- commands/color.js
+++ commands/color.js
@@ -14,13 +14,13 @@ module.exports = {
         const member = interaction.member;
         const guild = interaction.guild;
 
-        if (!/^[0-9A-F]{6}$/i.test(hex)) {
+        if (!/^[0-9A-F]{6}$/i.test(hex) || ["000000", "#000000"].includes(hex)) {
             embed.setColor("#FF0000")
                 .setTimestamp()
-                .setDescription(`Not a valid hex code.`)
+                .setDescription(`Not a valid hex code. (You can't set your color to black because of Discord's design.)`)
                 .setFooter({ text: "did a stupid", iconURL: member.user.avatarURL() });
         } else {
-            const customRole = member.roles.cache.filter(role => role.color !== 0 && role.name !== "@everyone").first();
+            const customRole = member.roles.cache.filter(role => role.color !== 0).first();
             if (customRole === undefined) {
                 member.roles.add(await guild.roles.create({
                     name: member.displayName,
@@ -28,10 +28,10 @@ module.exports = {
                     position: guild.roles.cache.find(role => role.name === "Bots" && role.color === 0).position + 1
                 }));
             }
-            console.log(typeof customRole)
+
             embed.setColor("#636363")
                 .setTimestamp()
-                .setDescription(`#${JSON.stringify(customRole)}.`)
+                .setDescription(`You changed <@&${customRole.id}'s color to ${hex}`)
                 .setFooter({ text: "changed their color", iconURL: interaction.member.user.avatarURL() });
         }
         return interaction.reply({ embeds: [embed], ephemeral: true });
