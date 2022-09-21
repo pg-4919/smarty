b9abfea290ee6d512c2ad127ad3605ed9e9110a0
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-21 16:45:59 -0400

    gay

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,24 +2,9 @@ const discord = require("discord.js");
 const utils = require("../utils/utils.js");
 const fs = require("fs");
 
-const spoofs = new discord.Collection();
-
-/*
-Notes:
-
-The spoof command uses the global storage variable spoofs,
-which is a discord collection with structure of { id: spoof }.
-A spoof is further broken down into { imposter: member, target: member }
-where both values are of type discord.GuildMember. 
-
-Furthermore, this command demonstrates two conventions used in 
-Smarty: subcommand parsing using switch statements and display of
-members in embeds using mentions.
-*/
-
 module.exports = {
     data: new discord.SlashCommandBuilder()
-        .setName("spoof")
+        .setName("avatar")
         .setDescription("See another person's avatar (or your own)")
         .addUserOption(option => option
             .setName("target")
