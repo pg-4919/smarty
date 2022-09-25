4fdcf70cd830f03a43a44e784ea3763cf7e1d814
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-25 14:21:05 -0400

    gay

---
+++ commands/restart.js
@@ -0,0 +1,27 @@
+const discord = require("discord.js");
+const utils = require("../utils/utils.js");
+
+module.exports = {
+    data: new discord.SlashCommandBuilder()
+        .setName("help")
+        .setDescription("Stop. Get some help.")
+        .toJSON(),
+        
+    async respond(interaction) {
+        const embed = new discord.EmbedBuilder()
+            .setColor("#2F3136")
+            .setTimestamp()
+            .setDescription(`Restarting ... may take a while.`)
+            .setFooter({ text: "restarted the bot", iconURL: interaction.member.user.avatarURL() });
+
+        await interaction.reply({ embeds: [embed], ephemeral: true });
+
+        return setTimeout(() => {
+            process.on("exit", () => {
+                require("child_process").exec(`cd ${utils.root} && npm start`)
+            });
+            process.exit();
+        }, 5000);
+    }
+}
+
