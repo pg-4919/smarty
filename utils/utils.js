6c0c8cdd73cd5f9e905c2d3810bf6bf944186368
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-09-28 12:12:03 -0400

    gay

--- utils/utils.js
+++ utils/utils.js
@@ -2,6 +2,5 @@ module.exports = {
     clone: require("./clone.js"),
     embed: require("./embed.js"),
     path: require("./path.js"),
-    roles: require("./roles.js"),
     root: require("./root.js")
 }
