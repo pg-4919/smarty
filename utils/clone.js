af16d33adfd291bed9e1e6055fadc5e2955e5ba5
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-01 11:01:00 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -1,6 +1,6 @@
 const discord = require("discord.js")
 
-module.exports = async (member, channel, message) => {
+module.exports = async (member, channel, message, ref = false) => {
     const webhooks = await channel.fetchWebhooks();
     const webhook = webhooks.first() || await channel.createWebhook({ name: "Smarty" });
     const attachments = message.attachments.toJSON();
@@ -8,7 +8,7 @@ module.exports = async (member, channel, message) => {
     try {
         await webhook.send({
             files: [...message.attachments.values()],
-            content: message.content + `\n[Jump](${message.url})` || "",
+            content: message.content + (ref ? `\n[Jump](${message.url})` : "") || "",
             username: member.displayName || "Poopy Dookykins",
             avatarURL: member.displayAvatarURL() || null,
             allowedMentions: { parse: [] }
