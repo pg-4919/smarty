de3adbf6bcc08ab68ef1f9ceb9dc8c2ee6fa8859
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-17 09:09:01 -0400

    gay

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,6 +2,6 @@ const utils = require("../utils/utils.js");
 
 module.exports = async (interaction) => {
     if (interaction.channel.name === "news") return;
-    if (interaction.isCommand()) interaction.client.commands.get(interaction.commandName).respond(interaction);
+    if (interaction.isCommand()) interaction.client.commands.get(interaction.commandName).respond(interaction).catch(err => console.log(err));
     if (interaction.isModalSubmit()) interaction.client.commands.get(interaction.customId.split("_")[0]).modal(interaction);
 }
