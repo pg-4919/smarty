b94bd66d46eb2a490d10f12559a52ba12c6873f1
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-17 21:52:12 -0400

    gay

--- events/ready.js
+++ events/ready.js
@@ -31,7 +31,7 @@ module.exports = async (client) => {
                 options.before = last_id;
             }
     
-            const messages = await channel.fetch(options);
+            const messages = await channel.messages.fetch(options);
             sum_messages.push(...messages.array());
             last_id = messages.last().id;
     
