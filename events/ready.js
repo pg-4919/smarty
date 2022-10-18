e47ba0f2d36bbede2aad8fba0327897e4f774df7
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-17 21:48:46 -0400

    ga

--- events/ready.js
+++ events/ready.js
@@ -19,4 +19,27 @@ module.exports = async (client) => {
 
     client.guilds.cache.each(guild => guild.commands.set(commands).catch(err => { console.log(err) }));
     client.application.commands.set(global);
+
+
+    async function getAll(channel, limit = 500) {
+        const sum_messages = [];
+        let last_id;
+    
+        while (true) {
+            const options = { limit: 100 };
+            if (last_id) {
+                options.before = last_id;
+            }
+    
+            const messages = await channel.fetchMessages(options);
+            sum_messages.push(...messages.array());
+            last_id = messages.last().id;
+    
+            if (messages.size != 100 || sum_messages >= limit) break;
+        }
+    
+        return sum_messages;
+    }
+
+    console.log(getAll(guild.channels.cache.find(channel => channel.name === "news")).length);
 }
