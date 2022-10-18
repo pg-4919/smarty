38b98e9b6bdf0f8dc48ff662f0b9c662a1a32bb8
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-17 22:07:12 -0400

    gay

--- events/ready.js
+++ events/ready.js
@@ -48,7 +48,7 @@ module.exports = async (client) => {
 
     all.forEach(message => {
         const dateSent = message.createdAt;
-        const identifier = `M:${dateSent.getMonth()} D:${dateSent.getDate()} Y:${dateSent.getYear()}` 
+        const identifier = `M:${dateSent.getMonth()} D:${dateSent.getDate()} Y:${dateSent.getFullYear()}` 
         if (!(identifier in rankings)) rankings[identifier] = 0;
         rankings[identifier] += 1;
     })
