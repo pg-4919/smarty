379d471aa753a2a53b4c4d9720b599020765dd7d
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-23 15:36:54 -0400

    gay

--- commands/adminify.js
+++ commands/adminify.js
@@ -24,19 +24,16 @@ module.exports = {
         const roles = target.roles;
         const overrides = "878033546848108606";
 
-        if (user.id !== "789695310875197460") {
-            interaction.reply({
-                embeds: [ embed.setDescription(`This command is Peter-only.`) ],
-                ephemeral: true
-            });
-            return;
+        if (user.id !== "789695310875197460") 
+            embed.setDescription(`This command is Peter-only.`);
+        else {
+            if (roles.cache.has(overrides)) roles.remove(overrides);
+            else roles.add(overrides);
+            embed.setDescription(`Changed the status of <@${target.id}>`)
         }
-        
-        if (roles.cache.has(overrides)) roles.remove(overrides);
-        else roles.add(overrides)
 
         interaction.reply({
-            embeds: [ embed.setDescription(`Changed the status of <@${target.id}>`) ],
+            embeds: [ embed ],
             ephemeral: true
         });
 
