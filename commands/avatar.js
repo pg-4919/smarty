e7bf928a28cd009d30caa70c3e1082e05a3e2d41
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-23 15:06:09 -0400

    gay

--- commands/avatar.js
+++ commands/avatar.js
@@ -25,7 +25,7 @@ module.exports = {
             .setColor("#2F3136")
             .setImage(avatarUrl)
             .setTimestamp()
-            .setFooter({ text: "** **", iconURL: member.displayAvatarURL() });
+            .setFooter({ text: " ", iconURL: member.displayAvatarURL() });
 
         interaction.reply({ embeds: [embed] });
 
