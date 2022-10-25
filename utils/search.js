ebf180c6ae95631116f4bc8beb2b54e4c55f8820
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-25 12:22:20 -0400

    gay

--- utils/search.js
+++ utils/search.js
@@ -1,3 +1,5 @@
 module.exports = {
-    byName = (collection, value) => collection.find(item => item.name === value)
+    byName(collection, value) {
+        return collection.find(item => item.name === value)
+    }
 }
