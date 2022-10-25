47cca2776edc381486f158c4671943bcbc8637e4
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-25 14:20:49 -0400

    gay

--- commands/role.js
+++ commands/role.js
@@ -33,19 +33,21 @@ module.exports = {
                 position: guild.roles.cache.get("813138438013452348").position + 1
             })).setColor("FFFFFF");
         member.roles.add(customRole);
-
-        embed.setDescription(`<@&${customRole.id}> updated.`);
         
         if (color) {
             if (!/^[0-9A-F]{6}$/i.test(color)) embed.setDescription(`Not a valid hex code.`)
             else { const hex = (color.replace("#", "") === "000000") ? "000001" : color.replace("#", ""); }
             customRole.setColor(hex);
         }
-
+        
         if (name) {
             if (name.length > 100) embed.setDescription(`Name must be 100 characters or fewer.`)
             else await customRole.setName(name);
         }
+
+        if (!color && !name) {
+            embed.setDescription(`<@&${customRole.id}> has name ${customRole.name} and color ${customRole.color}.`);
+        } else embed.setDescription(`<@&${customRole.id}> updated.`);
 
         interaction.reply({ embeds: [embed], ephemeral: true });
 
