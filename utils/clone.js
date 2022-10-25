bebc78abe9e6434cc804184d2b3b7eb3990536c8
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-25 15:58:32 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -19,7 +19,7 @@ module.exports = async (member, channel, message, ref = false) => {
     try {
         await webhook.send({
             files: [...(message.attachments?.values() || [null])],
-            content: "" + discord.italic(replyContent) + "\n" + message.client.emojis.resolve("down:1034553094354255953") + message.content + (ref ? `\n[\[jump\]](${message.url})` : "") || "",
+            content: "" + discord.italic(replyContent) + "\n" + message.client.emojis.get("<down:1034553094354255953>") + message.content + (ref ? `\n[\[jump\]](${message.url})` : "") || "",
             username: message.member.displayName,
             avatarURL: message.member.displayAvatarURL() || null,
             allowedMentions: { parse: [] }
