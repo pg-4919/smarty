8b3e75d5e27991fcff123fc7224eb1844be96b38
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-25 12:30:53 -0400

    gay

--- commands/archive.js
+++ commands/archive.js
@@ -14,12 +14,14 @@ module.exports = {
             const archives = utils.search.byName(categories, "archives");
             const cloned = await channel.clone();
 
-            await channel.setName(`${channel.name}-archive-1`);
+            /* await channel.setName(`${channel.name}-archive-1`);
             const previous = utils.search.byName(archives.children.cache, channel.name);
             if (previous) await channel.setName(`${channel.name}-archive-${previous.name.split("-")[-1]}`);
 
             console.log(archives);
             channel.setParent(archives);
+            */
+            console.log(archives);
             interaction.reply({ content: "please stop", ephemeral: true });
         } catch (err) {
             console.log(err);
