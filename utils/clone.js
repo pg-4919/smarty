7688d9fcf63cc34d93644caa308052280abbb0d7
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-25 14:49:33 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -5,12 +5,19 @@ module.exports = async (member, channel, message, ref = false) => {
     const webhook = webhooks.first() || await channel.createWebhook({ name: "Smarty" });
     const attachments = message.attachments?.toJSON();
     
+    let content = message.content;
+    if (message.reference && message.type === 19) {
+        const reference = message.reference;
+        const reply = message.channel.messages.fetch({ limit: 100 }).get(reference.messageId);
+        console.log(reply);
+    }
+
     try {
         await webhook.send({
             files: [...(message.attachments?.values() || [null])],
             content: message.content + (ref ? `\n[\[jump\]](${message.url})` : "") || "",
-            username: member.displayName || "Poopy Dookykins",
-            avatarURL: member.displayAvatarURL() || null,
+            username: message.member.displayName || "Poopy Dookykins",
+            avatarURL: message.member.displayAvatarURL() || null,
             allowedMentions: { parse: [] }
         });
     } catch (err) {
