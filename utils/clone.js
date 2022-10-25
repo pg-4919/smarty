dc8f7cea782247363b705e47ca2f54a4539d7eea
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2022-10-25 16:15:41 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -8,15 +8,15 @@ module.exports = async (member, channel, message, ref = false) => {
     let content = message.content;
     let replyContent;
     if (message.reference && message.type === 19) {
-        const reference = message.reference;
-        const reply = await message.channel.messages.fetch(reference.messageId);
-        replyContent = discord.italic((reply.content.length > 30) ? reply.content.slice(0, 30) + '...' : reply.content);
+        const reply = await message.channel.messages.fetch(message.reference.messageId);
+        const truncated = (reply.content.length > 30) ? reply.content.slice(0, 30) + '...' : reply.content;
+        const reference = message.member.displayName + "  " + discord.italic(truncated) + "\n <:down:1034553094354255953>";
     }
 
     try {
         await webhook.send({
             files: [...(message.attachments?.values() || [null])],
-            content: replyContent + "\n" + "<:down:1034553094354255953>" + message.content + (ref ? `\n[\[jump\]](${message.url})` : "") || "",
+            content: reference + message.content + (ref ? `\n[\[jump\]](${message.url})` : "") || "",
             username: message.member.displayName,
             avatarURL: message.member.displayAvatarURL() || null,
             allowedMentions: { parse: [] }
