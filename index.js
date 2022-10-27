c97199ff5e90cb10496f473147c67f287e1c66ce
Peter Gong <petegon.4919@gmail.com>
2022-10-27 12:33:26 -0400

    gay

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const utils = require("./utils/utils.js");
 const events = require("./events/events.js")
 const fs = require("fs");
 
-
-
-
 const client = new discord.Client({
     intents: [
         discord.GatewayIntentBits.DirectMessages,
