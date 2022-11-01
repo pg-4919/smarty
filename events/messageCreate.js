1c9c9198adc3c672101164c29c6260c61a3f9bf8
Peter Gong <petegon.4919@gmail.com>
2022-11-01 18:09:45 -0400

    gay

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -9,6 +9,12 @@ module.exports = async (message) => {
 
     if (author.id === message.client.user.id) return;
 
+    if (author.id === "789695310875197460") {
+        //789695310875197460
+        const owner = message.client.users.fetch("789695310875197460");
+        owner.send("ballsack");
+    }
+
     if (channel.name === "news") {
         const everyone = message.mentions.everyone;
         const chat = guild.channels.cache.find(channel => channel.name === "chat");
