97922098ecbc4d9ff71a88de42725174a3bd00b0
Peter Gong <petegon.4919@gmail.com>
2022-11-04 12:14:24 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -1,34 +1,35 @@
-const discord = require("discord.js")
+const discord = require("discord.js");
+const truncate = require("./truncate.js");
 
 module.exports = async (member, channel, message, ref = false) => {
     const webhooks = await channel.fetchWebhooks();
     const webhook = webhooks.first() || await channel.createWebhook({ name: "Smarty" });
     const attachments = message.attachments?.toJSON();
-    
+
     let content = message.content;
     let reference = "";
 
     if (message.reference && message.type === 19) {
         const reply = await message.channel.messages.fetch(message.reference.messageId);
-        const truncated = utils.truncate(reply.content.length);
-        const original = reply.member || reply.guild.members.fetch(reply.user.id);
+        const truncated = truncate(reply.content, 30);
+        const original = await reply.guild.members.fetch(reply.author.id);
+        reply.guild.members.fetch();
         console.log(original);
+
         reference = "<:curved:1034653422416302151> "
-            + discord.bold(await (message.guild.members.cache.find(reply.author.id))?.displayName || reply.author.username)
+            + discord.bold(original.displayName)
             + "  " + truncated
             + "\n<:straight:1034653871613681714>\n ";
     }
 
-    try {
-        await webhook.send({
-            files: [...(message.attachments?.values() || [null])],
-            content: reference + message.content + (ref ? `\n[\[jump\]](${message.url})` : "") || "",
-            username: message.member.displayName,
-            avatarURL: message.member.displayAvatarURL() || null,
-            allowedMentions: { parse: [] },
-            embeds: [ ...message.embeds ]
-        });
-    } catch (err) {
-        console.log(err);
-    }
+    webhook.send({
+        files: [...(message.attachments?.values() || [null])],
+        content: reference + message.content + (ref ? `\n[\[jump\]](${message.url})` : "") || "",
+        username: message.member.displayName,
+        avatarURL: message.member.displayAvatarURL() || null,
+        allowedMentions: { parse: [] },
+        embeds: [...message.embeds]
+    }).catch(err => console.log);
+
+    return;
 }
