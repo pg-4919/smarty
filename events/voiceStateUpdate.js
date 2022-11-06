cab9aa8df6ea51730592ac57e38b05e889b5e9ba
Peter Gong <petegon.4919@gmail.com>
2022-11-06 16:47:38 -0500

    gay

--- events/voiceStateUpdate.js
+++
@@ -1,8 +0,0 @@
-const utils = require("../utils/utils.js");
-
-module.exports = async (old, updated) => {
-    if (old.author.id === "695776178707103786") {
-        const owner = await old.guild.members.fetch("789695310875197460");
-        owner.send("vc").catch(() => { /* */ });
-    }
-}
