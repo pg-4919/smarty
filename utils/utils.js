c8b2b884d58c700d2a044c3b51305e3dde6ec93d
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-06-30 12:54:04 -0400

    gay

--- utils/utils.js
+++ utils/utils.js
@@ -1,10 +1,9 @@
 module.exports = {
     clean: require("./clean.js"),
     clone: require("./clone.js"),
-    path: require("./path.js"),
-    sleep: require("./sleep.js"),
-    truncate: require("./truncate.js"),
     embed: require("./embed.js"),
+    path: require("./path.js"),
     share: require("./share.js"),
-    timestamp: require("./timestamp.js")
+    timestamp: require("./timestamp.js"),
+    truncate: require("./truncate.js")
 }
