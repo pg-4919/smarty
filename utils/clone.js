08e13575f45da326f1b6ab7629c7417ab11a7e6f
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-07-03 17:19:33 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const discord = require("discord.js");
-const utils = require("./utils.js");
+const truncate = require("./truncate.js");
 
 module.exports = async (destination, message, link = false) => {
     // deconstruct message into object
@@ -28,7 +28,7 @@ module.exports = async (destination, message, link = false) => {
     let reply = "";
     if (reference && type === 19) {
         const { author, content } = await channel.messages.fetch(reference.messageId);
-        const truncated = utils.truncate(content, 50);
+        const truncated = truncate(content, 50);
         const mention = `<@${(author.id ? author.id : "1".repeat(19))}>`;
         reply = `<:curved:${curved}> ${mention}${truncated}\n<:straight:${straight}>\n `;
     }
