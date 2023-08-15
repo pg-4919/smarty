5f1ff9a395730fe770bee7c250cd0d61d9cbed78
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-08-15 14:14:31 -0400

    gay

--- events/ready.js
+++ events/ready.js
@@ -4,28 +4,34 @@ const discord = require("discord.js");
 const utils = require("../utils/utils.js");
 const fs = require("fs");
 
-module.exports = async client => {
-    client.commands = new discord.Collection();
-
+async function updateCommands() {
     const commands = [];
     const files = fs.readdirSync(utils.path.commands);
 
-    for (const file of files) {
+    files.forEach(file => {
         const command = require(`${utils.path.commands}/${file}`);
         client.commands.set(command.data.name, command)
         commands.push(command.data);
-    }
+    });
 
-    // update the commands (instant updates)
     const guild = await client.guilds.fetch(client.config.guild);
     await guild.commands.set(commands);
+}
 
-    // fetch all relevant information into cache
+async function updateCaches() {
     await guild.members.fetch();
     await guild.roles.fetch();
     await guild.channels.fetch();
     await guild.invites.fetch();
     await guild.emojis.fetch();
+}
+
+module.exports = async client => {
+    client.commands = new discord.Collection();
+    client.clones = new discord.Collection();
+
+    await updateCommands();
+    await updateCaches();
 
     guild.roles.cache
         .filter(role => role.members.size === 0)
@@ -33,5 +39,5 @@ module.exports = async client => {
 
     await client.user.setActivity("with fire", { type: 0 });
 
-    return;
+    return console.log("Ready to domestically terrorize multiple nation states");
 }
