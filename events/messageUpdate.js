2a31aa79170e2e6d833570361c905147ecd367be
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-08-15 15:25:27 -0400

    gay

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -4,5 +4,6 @@ module.exports = async message => {
     const { author, channel, client, guild } = message;
     const { news, chat } = client.config.channels;
 
-    if (client.cloned.has(message.id)) console.log("oh Shit!")
+    if (client.clones.has(message.id)) console.log("oh Shit!")
+    
 }
