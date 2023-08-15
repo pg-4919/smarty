2d5af70fcb74bf6682f902df71013968a5543b9b
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-08-15 14:26:45 -0400

    gay

--- utils/clone.js
+++ utils/clone.js
@@ -6,13 +6,13 @@ const truncate = require("./truncate.js");
 async function buildReply(reference, type) {
     const { curved, straight } = client.config.emojis;
 
-    if (reference && type === 19) {
-        const { author, content : rawContent } = await channel.messages.fetch(reference.messageId);
-        const content = clean(truncate(rawContent, 50));
-        const mention = `<@${(author.id ? author.id : "1".repeat(19))}>`;
-        const reply = `<:curved:${curved}>  ${mention}${truncated}\n<:straight:${straight}>\n `;
-        return reply;
-    } else return undefined;
+    if (!(reference && type === 19)) return undefined;
+
+    const { author, content: rawContent } = await channel.messages.fetch(reference.messageId);
+    const content = clean(truncate(rawContent, 50));
+    const mention = `<@${(author.id ? author.id : "1".repeat(19))}>`;
+    const reply = `<:curved:${curved}>  ${mention}${truncated}\n<:straight:${straight}>\n `;
+    return reply;
 }
 
 module.exports = async (destination, message, link = false) => {
@@ -31,22 +31,23 @@ module.exports = async (destination, message, link = false) => {
 
     const webhooks = await destination.fetchWebhooks();
     const webhook = webhooks.first() || await destination.createWebhook({ name: "Smarty" });
-    
+
+    const link = [new discord.ActionRowBuilder().addComponents(
+        new discord.ButtonBuilder()
+            .setLabel("Original message")
+            .setStyle(discord.ButtonStyle.Link)
+            .setURL(url))]
+
     return await webhook.send({
         allowedMentions: { parse: [] },
+        embeds: [...embeds],
+        flags: [4096],
+
         avatarURL: member?.displayAvatarURL() || null,
         content: `${await buildReply(reference, type) || ""}${content}` || "",
-        embeds: [...embeds],
-        files: [...(attachments?.values() || [null])],
         username: member?.displayName || author?.username || "Anonymous",
-        flags: [4096],
-        components: link ? [new discord.ActionRowBuilder().addComponents(
-            new discord.ButtonBuilder()
-                .setLabel("Original message")
-                .setStyle(discord.ButtonStyle.Link)
-                .setURL(url)
-        )
-        ] : []
+
+        components: link || []
     }).catch(err => console.log);
 
 }
