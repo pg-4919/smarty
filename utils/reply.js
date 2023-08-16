7c1cc2bd267787fc4ce292cc43751cd45784290b
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-08-15 20:38:19 -0400

    gay

--- utils/reply.js
+++ utils/reply.js
@@ -10,6 +10,10 @@ module.exports = async message => {
     const original = await channel.messages.fetch(reference.messageId);
     const content = clean(truncate(original.content, 50));
     const mention = `<@${(original.author.id || "1".repeat(19))}>`;
-    const reply = `<:curved:${curved}> ${mention} [${content}](${original.url})\n<:straight:${straight}>\n `;
+
+    if (client.clones.has(original.id))
+        return `<:curved:${curved}> ${mention} [${content}](${client.clones.get(original.id).url})\n<:straight:${straight}>\n `;
+    else
+    return `<:curved:${curved}> ${mention} [${content}](${original.url})\n<:straight:${straight}>\n `;
     return reply;
 }
