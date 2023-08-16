19acc4f423321c3d4894499fca73695d001cb313
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-08-15 20:35:10 -0400

    gay

--- utils/clean.js
+++ utils/clean.js
@@ -2,6 +2,6 @@
 
 const discord = require("discord.js");
 
-module.exports = content => discord.escapeMarkdown(content);
+module.exports = content => discord.cleanContent(discord.escapeMarkdown(content));
 
 //content.replace(/([*])|(\|{2,})|(_{2,})|(^(> ))/g, "").replace(/\n/g, "");
