fc0fe6cae0c406ff15a80da816f968bb1b5058d2
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2023-08-20 09:56:37 -0400

    gay

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -3,7 +3,6 @@ const utils = require("../utils/utils.js");
 module.exports = async reaction => {
     const { client, emoji, message } = reaction;
     const oreo = message.guild.members.fetch({ user: "1025778682394058772", withPresences: true });
-    oreo.fetch(true);
 
     if (oreo.presence.status !== "online") {
         const starred = await message.guild.channels.fetch(client.config?.channels.starred);
