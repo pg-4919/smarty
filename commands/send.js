5d3501bedae69d61aa076b81643b7a6a5c53e69a
pg-4919 <77947679+pg-4919@users.noreply.github.com>
2024-02-06 14:40:43 -0500

    gay

---
+++ commands/send.js
@@ -0,0 +1,30 @@
+const discord = require("discord.js");
+const utils = require("../utils/utils.js");
+
+module.exports = {
+    data: new discord.SlashCommandBuilder()
+        .setName("send")
+        .setDescription("Send a message as our almighty Daddy")
+        .addChannelOption(option => option.setName("channel")
+            .setDescription("Where to send it")
+            .setRequired(false)
+        )
+        .addStringOption(option => option.setName("message")
+            .setDescription("What to send")
+            .setRequired(true)
+        )
+        .toJSON(),
+
+    async respond(interaction) {
+        const { user, options, client } = interaction;
+        
+        const channel = options.getMember("channel") || interaction.channel;
+        const message = options.getString("message");
+
+        if (!client.config.admins.includes(user.id))
+            embed.setDescription(`You don't have permission to use this command.`);
+        else {
+            channel.send(message.content);
+        }
+    },
+}
